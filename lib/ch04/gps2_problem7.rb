22a3205ac407b28d07d56e00f2edd8d89e998446
Ola Bini <ola.bini@gmail.com>
2008-09-14 10:22:30 +0200

    Add example of problem that really can't be solved well

---
+++ lib/ch04/gps2_problem7.rb
@@ -0,0 +1,15 @@
+require 'gps3'
+require 'pp'
+
+gps = GPS2.new([:son_at_home, 
+                :have_money, 
+                :have_works],
+              GPS::SCHOOL_OPS + 
+               [
+                GPS2::op(:taxi_son_to_school,
+                         :preconds => [:son_at_home, :have_money],
+                         :add_list => [:son_at_school],
+                         :del_list => [:son_at_home, :have_money])
+               ])
+debug :gps
+pp gps.solve(:son_at_school, :have_money)
