0dc3dda276acaae5eefb5d926807e71845f69062
Ola Bini <ola.bini@gmail.com>
2008-09-30 07:50:53 +0200

    Finalize more sophisticated pattern matching

--- lib/ch06/pat_match.rb
+++ lib/ch06/pat_match.rb
@@ -4,9 +4,8 @@ class Pattern
   end
 
   def match(input, bindings = {}, pattern = @pattern)
-    p [:match, input, bindings, pattern]
     case
-    when bindings.nil?
+    when !bindings
       nil
     when variable?(pattern)
       match_variable(pattern, input, bindings)
@@ -29,9 +28,9 @@ class Pattern
     pattern.is_a?(String) && pattern[0] == ??
   end
 
-  def match_variable(p, i, bindings)
-    (bindings[p].nil? && bindings.merge(p => i)) ||
-      (bindings[p] == i && bindings)
+  def match_variable(px, i, bindings)
+    (!bindings[px] && bindings.merge(px => i)) ||
+      (bindings[px] == i && bindings)
   end
 
   def segment_pattern?(pattern)
@@ -71,7 +70,7 @@ class Pattern
 
   def match_and(patterns, input, bindings)
     case 
-    when bindings.nil?
+    when !bindings
       nil
     when patterns.empty?
       bindings
@@ -89,7 +88,7 @@ class Pattern
       nil
     else
       new_bindings = match(patterns[0], input, bindings)
-      if new_bindings.nil?
+      if !new_bindings
         match_or(patterns[1..-1], input, bindings)
       else
         new_bindings
@@ -106,6 +105,48 @@ class Pattern
   end
   
   def segment_match(pattern, input, bindings, start = 0)
+    var = pattern[0][1]
+    pat = pattern[1..-1]
+    if !pat || pat.empty?
+      match_variable(var, input, bindings)
+    else
+      pos = first_match_pos(pat[0], input, start)
+      return nil unless pos
+
+      b2 = match(input[pos..-1], 
+                 match_variable(var, input[0...pos], bindings), 
+                 pat)
+      return b2 if b2
+      segment_match(pattern, input, bindings, pos+1)
+    end
+  end
+  
+  def first_match_pos(pat1, input, start)
+    input = [] unless input
+    case
+    when !pat1.is_a?(Array) && !variable?(pat1)
+      (vv = input[start..-1].index(pat1)) && vv + start
+    when start < input.length
+      start
+    else
+      nil
+    end
+  end
+
+  def segment_match_plus(pattern, input, bindings)
+    segment_match(pattern, input, bindings, 1)
+  end
+
+  def segment_match_?(pattern, input, bindings)
+    var = pattern[0][1]
+    pat = pattern[1..-1]
+    match(input, bindings, [var] + pat) ||
+      match(input, bindings, pat)
+  end
+
+  def match_if(pattern, input, bindings)
+    # Not implemented because of the current structure of the matching
+    false
   end
   
   SINGLE_MATCH = {
@@ -118,11 +159,17 @@ class Pattern
   SEGMENT_MATCH = { 
     "?*"  => :segment_match,
     "?+"  => :segment_match_plus,
-    "??"  => :segment_match?,
+    "??"  => :segment_match_?,
     "?if" => :match_if
   }
 end
 
 require 'pat_read'
-p Pattern.from_string("a (?* ?x) d")
 p Pattern.from_string("a (?* ?x) d").match(%w(a b c d))
+p Pattern.from_string("a (?* ?x) (?* ?y) d").match(%w(a b c d))
+p Pattern.from_string("a (?* ?x) (?* ?y) ?x ?y").match(['a', 'b', 'c', 'd', ['b', 'c'], ['d']])
+
+Pattern.match_abbrev :"?x*", %w(?* ?x)
+Pattern.match_abbrev :"?y*", %w(?* ?y)
+
+p Pattern.from_string("a ?x* b")
--- lib/ch06/pat_read.rb
+++ lib/ch06/pat_read.rb
@@ -18,8 +18,29 @@ class Pattern
           result << f[0][1..-2].split
         end
       end
-      Pattern.new result
+      Pattern.new expand(result)
+    end
+
+    attr_accessor :expansions
+
+    def match_abbrev(symbol, expansion)
+      self.expansions[symbol.to_sym] = expand(expansion)
+    end
+
+    def expand(pat)
+      if !pat.is_a?(Array)
+        if self.expansions[pat.to_sym]
+          self.expansions[pat.to_sym]
+        else
+          pat
+        end
+      elsif pat.empty?
+        pat
+      else
+        [expand(pat[0])] + expand(pat[1..-1])
+      end
     end
   end
-end
 
+  self.expansions = { }
+end
